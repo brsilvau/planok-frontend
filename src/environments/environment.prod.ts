bae2ecce83bb3ffe0c3c0b8780b4a5f02ef88ea5
brsilvau <brsilvau@gmail.com>
2025-03-27 13:32:23 -0300

    force api variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: process.env['NG_APP_API_URL'] || 'https://planok-backend.onrender.com/api'
+    apiUrl: 'https://planok-backend.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: process.env['NG_APP_API_URL'] || 'https://planok-backend.onrender.com/api'
+    apiUrl: 'https://planok-backend.onrender.com/api'
 };
